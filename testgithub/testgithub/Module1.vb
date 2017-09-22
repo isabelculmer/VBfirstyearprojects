1599081376028399ee369fa0add02c165f154867
IMC <IMC@barton.ac.uk>
2017-09-22 10:31:33 +0100

    made a change

--- testgithub/testgithub/Module1.vb
+++ testgithub/testgithub/Module1.vb
@@ -2,6 +2,13 @@
 
     Sub Main()
         Console.WriteLine("addign a change")
+        Console.WriteLine("addign a change")
+        Console.WriteLine("addign a change")
+        Console.WriteLine("addign a change")
+        For i = 1 To 10
+            Console.WriteLine("addign a change")
+        Next
+
     End Sub
 
 End Module
